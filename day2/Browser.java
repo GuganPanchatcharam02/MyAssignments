2d9bf6a0547609815331fcbbdeb2aaad59234b46
GuganPanchatcharam02 <guganpanchatcharam02@gmail.com>
2025-05-05 21:22:05 +0530

    Add files via upload

---
+++ day2/Browser.java
@@ -0,0 +1,29 @@
+package week1.day2;
+//. Create a Java class named Browser
+public class Browser {
+	
+	public void launchBrowser() {
+		String lb= "Browser";
+	System.out.println("Browser launched successfully");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String lb1="loadurl";
+		Browser B= new Browser();
+		
+		System.out.println("Application url loaded successfully");
+		B.launchBrowser();
+		
+		
+			
+		// -Add return statement to return the browserName
+		// -Create a method as loadUrl()
+		 //-Print the statement "Application url loaded successfully"
+		//2. Create an object for Browser Class in the main method, call two methods, and then run the code
+		//3. Create another Class EdgeBrowser and create an object for Browser in the main method and call 
+		//both methods of Browser class of Browser class from EdgeBrowser and execute
+		
+
+	}
+
+}
---
+++ day2/Primitive_Datatype.java
@@ -0,0 +1,53 @@
+package week1.day2;
+
+public class Primitive_Datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//integer
+		int i=17;
+		System.out.println(i);
+		
+		//Byte
+		byte b=127;
+		System.out.println(b);
+		
+		//short
+		short s=8788;
+		System.out.println(s);
+		
+		//long
+        long phno=8072222300l;
+        System.out.println(phno);
+		
+		//float
+		float fl=7.689767878876f;
+		System.out.println(fl);
+		
+		//double
+		double d1=7.68976787810876f;
+		System.out.println(d1);
+		
+		//char
+		char jr='G';
+		System.out.println(jr);
+		
+		//non primitive datatype.
+		// String
+		String Name="Gugan";
+		System.out.println(Name);
+		
+				
+		
+		
+		
+		
+			
+		
+			
+		}
+		
+
+	}
+
